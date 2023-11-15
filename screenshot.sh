19479229497ffae48dac02dc29ad06fea8627cd9
Fishsticks / PureSesh <150125982+puresesh@users.noreply.github.com>
2023-11-15 11:56:34 +0100

    Update screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-scrot -e 'xclip -selection clipboard -t image/png -i $f' --select --line mode=edge
+output='/home/[username]/pictures/screenshots/%Y-%m-%d-%T-screenshot.png'
+
+#scrot "$output" --select --line mode=edge
+
+scrot "$output" -e 'xclip -selection clipboard -t image/png -i $f' --select --line mode=edge
